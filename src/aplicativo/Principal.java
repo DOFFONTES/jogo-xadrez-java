ced9f376c0b23dcfe2263e92822c078d42710224
David Fontes <doffontes@gmail.com>
2020-07-24 18:50:21 -0300

    Primeira classe Posicao

---
+++ src/aplicativo/Principal.java
@@ -0,0 +1,13 @@
+package aplicativo;
+
+import tabuleiro.Posicao;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Posicao pos = new Posicao(3, 2);
+		System.out.println(pos.toString());
+	}
+
+}
